88cd707c0fe9654f634fe53d7715f94b4a1915fb
Aleksey Chernykh <57570945+Chernykh-a-s@users.noreply.github.com>
2023-02-18 19:01:00 +0300

    Update lists_dicts.py

--- lists_dicts.py
+++ lists_dicts.py
@@ -11,15 +11,15 @@ lst.remove('Python')
 
 
 dict1 = {
-    "city": "Москва",
-    "temperature": "20",
+    'city': 'Москва',
+    'temperature': '20',
 }
-print(dict1["city"])
-dict1["temperature"] = 15
+print(dict1['city'])
+dict1['temperature'] = 15
 print(dict1)
 
 
 print(dict1.get('country'))
-print(dict1.get('country', "Россия"))
-dict1['date'] = "27.05.2019"
+print(dict1.get('country', 'Россия'))
+dict1['date'] = '27.05.2019'
 print(len(dict1))
